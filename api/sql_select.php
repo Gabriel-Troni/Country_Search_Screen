3bb970708feccac8705cba8eaa59d0bf53c4ebba
Gabriel Troni <troni.gabriel.10@gmail.com>
2024-01-19 01:45:46 -0300

    update sql select

--- api/sql_select.php
+++ api/sql_select.php
@@ -3,12 +3,14 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
     $sql = "SELECT Country.name AS pais, 
                 City.name AS capital,
                 Country.population AS populacao,
-                CountryLanguage.language  AS lingua                 
+                GROUP_CONCAT(CountryLanguage.language SEPARATOR ', ') AS lingua                
         FROM    Country
                 INNER JOIN City 
                     ON Country.capital = City.ID
                 INNER JOIN CountryLanguage 
-                    ON Country.code = CountryLanguage.CountryCode LIMIT 50";
+                    ON Country.code = CountryLanguage.CountryCode 
+        GROUP BY Country.name, City.name, Country.population
+        LIMIT 50";
 } else if ($_SERVER["REQUEST_METHOD"] == "POST") {
     require("check_form.php");
     $limite = ($qtde != "" && $qtde > 0) ? "LIMIT $qtde" : "";
