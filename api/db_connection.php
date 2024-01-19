92fb0043cd6228d0311329fb9755cadaf12c7cf7
Gabriel Troni <troni.gabriel.10@gmail.com>
2024-01-19 01:21:32 -0300

    debug

--- api/db_connection.php
+++ api/db_connection.php
@@ -1,10 +1,13 @@
-<?php 
+<?php
+  $db_ca        = $_ENV["PLANETSCALE_SSL_CERT_PATH"];
   $servername   = $_ENV["PLANETSCALE_DB_HOST"];
   $username     = $_ENV["PLANETSCALE_DB_USERNAME"];
   $db_password  = $_ENV["PLANETSCALE_DB_PASSWORD"];
   $dbname       = $_ENV["PLANETSCALE_DB"];
 
-  $conn = mysqli_connect($servername, $username, $db_password);
+  $conn = mysqli_init();
+  mysqli_ssl_set($conn, NULL, NULL, $db_ca, NULL, NULL);
+  mysqli_real_connect($conn, $servername, $username, $db_password, $dbname, NULL, NULL, MYSQLI_CLIENT_SSL);
   if(!$conn){
     die("Erro na conexão: " . mysqli_connection_error());
   }
